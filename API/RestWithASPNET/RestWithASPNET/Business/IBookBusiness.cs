bfd385a260deb129d65230241deee73577924771
Fabrício M. Damasceno <fabricio.damasceno@outlook.com.br>
2023-09-04 16:30:38 -0300

    Adicionando API ao projeto.

---
+++ API/RestWithASPNET/RestWithASPNET/Business/IBookBusiness.cs
@@ -0,0 +1,17 @@
+﻿using RestWithASPNET.Data.VO;
+using RestWithASPNET.Hypermedia.Utils;
+using System.Collections.Generic;
+
+namespace RestWithASPNET.Business
+{
+    public interface IBookBusiness
+    {
+        BookVO Create(BookVO book);
+        BookVO FindByID(long id);
+        List<BookVO> FindAll();
+        PagedSearchVO<BookVO> FindWithPagedSearch(
+            string title, string sortDirection, int pageSize, int page);
+        BookVO Update(BookVO book);
+        void Delete(long id);
+    }
+}
